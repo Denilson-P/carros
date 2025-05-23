9cd1ee986d77a535496b9a30c4a9aa2ecf086099
denison <denilsonsantos00@gmail.com>
2025-05-23 04:21:25 -0300

    frist commit

---
+++ cars/serializers.py
@@ -0,0 +1,14 @@
+"""from rest_framework import serializers
+
+from .models import Brand, Car
+
+class BreandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = "__all__"
+
+
+class CarSerailaizer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = "__all__"""
---
+++ cars/urls.py
@@ -0,0 +1,8 @@
+"""from rest_framework.routers import SimpleRouter
+
+from .views import BrandViewset, CarViewset
+
+router = SimpleRouter()
+router.register("brand", BrandViewset)
+router.register("car", CarViewset)
+"""
